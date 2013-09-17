73c1034a423199b1382442f6cc7e89a2d1614f35
Héctor <hector0193@gmail.com>
2013-09-17 19:20:56 +0200

    Almost solved.

--- X56972_Multiseleccion/main.cc
+++ X56972_Multiseleccion/main.cc
@@ -23,8 +23,8 @@ template<class T> int partition(vector<T> &elements, int start, int end)
         while(i <= j and elements[i] <= p)
             i++;
         
-        while(i < j and elements[j] >= p)
-            j++;
+        while(i <= j and elements[j] >= p)
+            j--;
         
         if(i <= j)
             myswap<T>(elements[i], elements[j]);
@@ -36,42 +36,46 @@ template<class T> int partition(vector<T> &elements, int start, int end)
 }
 
 // Cost: n log s
-template<class T> int quickselect(vector<T> &elements, int select, int start, int end)
+template<class T> T quick_select(vector<T> &elements, int k, int start, int end)
 {
     if(start == end)
-        return elements[start];
+        return elements[end];
+
+    int j = partition(elements, start, end);
+
+    if(k == j)
+        return elements[j];
     
-    int k = partition<T>(elements, start, end);
-    
-    if(select == k)
-        return elements[k];
-    
-    if(select < k)
-        return quickselect<T>(elements, start, select, k - 1);
-    
-    return quickselect<T>(elements, k + 1, select, end);    
+    if(k < j)
+        return quick_select(elements, k, start, j-1);
+
+    return quick_select(elements, k, j+1, end);
 }
 
 // Cost: n log p
-template<class T> void mselect(vector<T> &elements, const vector<int> &ranges, int elementStart, int elementEnd,
-        int rangeStart, int rangeEnd)
+template<class T> void mselect(vector<T> &elements, const vector<int> &ranges,
+        int elementStart, int elementEnd, int rangeStart, int rangeEnd)
 {
-    int rangesSize = rangeEnd - rangeStart;
+    int rangesSize = rangeEnd - rangeStart + 1;
     
-    if(rangesSize <= 0)
+    if(rangesSize < 1)
         return;
     
     if(rangesSize == 1)
-        quickselect<T>(elements, ranges[0], elementStart, elementEnd);
+    {
+        cout << rangeStart << endl;
+        quick_select<T>(elements, ranges[rangeStart] + 1 - elementStart, elementStart, elementEnd);
+    }
     else
     {
-        int p = rangeStart + rangesSize / 2;
-        int pivot = ranges[p];
+        int middle = rangeStart + rangesSize / 2;
+        cout << middle << endl;
+        int pivot = ranges[middle];
         
-        quickselect<T>(elements, pivot, elementStart, elementEnd);
+        quick_select<T>(elements, pivot + 1 - elementStart, elementStart, elementEnd);
         
-        mselect<T>(elements, ranges, elementStart, p-1, rangeStart, p-1);
-        mselect<T>(elements, ranges, p+1, elementEnd, p+1, rangeEnd);
+        mselect<T>(elements, ranges, elementStart, middle-1, rangeStart, middle-1);
+        mselect<T>(elements, ranges, middle+1, elementEnd, middle+1, rangeEnd);
     }
 }
 
@@ -90,10 +94,15 @@ int main()
     for(int i = 0; i < n; ++i)
         cin >> elements[i];
     
-    mselect<int>(elements, ranges, 0, elements.size(), 0, ranges.size());
+    mselect<int>(elements, ranges, 0, elements.size() - 1, 0, ranges.size() - 1);
     
     for(int i = 0; i < p; ++i)
-        cout << elements[ranges[i]];
+    {
+        if(i != 0)
+            cout << ' ';
+        
+        cout << elements[ranges[i] - 1];
+    }
     
     cout << endl;
     
